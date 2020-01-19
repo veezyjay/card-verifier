0c0afc4d367ee73a06de401bf28572bfae795055
Victor Bassey <viktorbasi545@gmail.com>
2020-01-20 00:01:01 +0100

    Add exception for cards that cannot be found

---
+++ src/main/java/com/github/veezyjay/cardverifier/exception/CardNotFoundException.java
@@ -0,0 +1,18 @@
+package com.github.veezyjay.cardverifier.exception;
+
+public class CardNotFoundException extends RuntimeException {
+    public CardNotFoundException() {
+    }
+
+    public CardNotFoundException(String message) {
+        super(message);
+    }
+
+    public CardNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CardNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
