647e79fd68bcd98d13ace1ae77aa51f166cfa3e5
Victor Bassey <viktorbasi545@gmail.com>
2020-01-19 14:49:35 +0100

    Setup project with maven
    Closes #1

---
+++ src/main/java/com/github/veezyjay/cardverifier/CardVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.github.veezyjay.cardverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardVerifierApplication.class, args);
+    }
+
+}
